57c986262dfba9d5f1f9a8a02c74b8320c84764d
SunSet0418 <coffee@soylatte.kr>
2018-03-10 02:06:49 +0900

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by SungWoon Cho on 2018. 3. 10..
+//  Copyright © 2018년 SunSet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
